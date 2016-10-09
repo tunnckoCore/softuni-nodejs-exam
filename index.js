68e927c6c9411e55d401ace9dff5a9c0a919dba6
tunnckoCore <mameto_100@mail.bg>
2016-10-09 07:28:05 +0300

    chore(*): some pushes

--- index.js
+++ index.js
@@ -7,40 +7,87 @@
 
 'use strict'
 
-/**
- * Extract to `koa-bel`
- */
-
-var path = require('path')
-
-function koaBel (dir, opts) {
-  if (typeof dir !== 'string') {
-    throw new TypeError('koa-bel: expect `dir` to be string')
-  }
-  opts = typeof opts === 'object' ? opts : {}
-  opts.cwd = typeof opts.cwd === 'string' ? opts.cwd : process.cwd()
-  dir = path.resolve(opts.cwd, dir)
-
-  return function (ctx, next) {
-    ctx.render = function render (fp, state) {
-      var view = require(path.join(dir, fp))
-      ctx.body = view(state).toString('utf8')
-      return ctx
-    }
-    return next()
-  }
-}
-
+var bel = require('bel')
 var Koa = require('koa')
+var views = require('koa-bel')
 var app = new Koa()
 
-app.use(koaBel('./views'))
-app.use(function (ctx, next) {
-  ctx.render('home2.js', {
-    title: 'Homepage',
-    description: 'This is example of `koa-bel` awesomeness!! :D',
-    items: ['grizzly', 'cat', 'frog']
+var body = require('koa-better-body')
+var extend = require('extend-shallow')
+var Resource = require('koa-resource-router')
+
+// GET     /todos             ->  index
+// GET     /todos/new         ->  new
+// POST    /todos             ->  create
+// GET     /todos/:user       ->  show
+// GET     /todos/:user/edit  ->  edit
+// PUT     /todos/:user       ->  update
+// DELETE  /todos/:user       ->  destroy
+
+var CONFIG = {
+  project: 'Restful Koa',
+  title: 'Welcome page',
+  description: 'This is funky landing page, bro!',
+  menu: [
+    { text: 'Home', link: '/' },
+    { text: 'Create', link: '/todos/new' },
+    { text: 'List', link: '/todos' }
+  ],
+  db: {}
+}
+
+app
+  .use(views('./views'))
+  .use(body({
+    uploadDir: './images'
+  }))
+  .use(function (ctx, next) {
+    if (ctx.url === '/') {
+      ctx.render('index', extend(CONFIG, {
+        title: 'Welcome page',
+        description: 'This is funky landing page, bro!'
+      }))
+    }
+    return next()
   })
-  return next()
+
+var todos = new Resource('todos', {
+  // GET /todos
+  index: function * (next) {
+    this.render('todos/index', extend(CONFIG, {
+      title: 'List of TODOs',
+      description: `Fancy list of all TODOs in the in-memory database.
+      The "pending" TODOs are above, "done" below.
+      `,
+      todos: []
+    }))
+  },
+  // GET /todos/new
+  new: function * (next) {
+    this.render('todos/new', extend(CONFIG, {
+      title: 'Create new TODO',
+      description: `We basically can add TODO with a title,
+      description and state. If state is not given, it is "pending"
+      by default. Later we will be able to upload "file" to each TODO.`,
+      form: bel`sasa`.toString()
+    }))
+  }
+  // // POST /todos
+  // create: function * (next) {
+  // },
+  // // GET /todos/:id
+  // show: function * (next) {
+  // },
+  // // GET /todos/:id/edit
+  // edit: function * (next) {
+  // },
+  // // PUT /todos/:id
+  // update: function * (next) {
+  // },
+  // // DELETE /todos/:id
+  // destroy: function * (next) {
+  // }
 })
-app.listen(4433)
+
+app.use(todos.middleware())
+app.listen(4290)
---
+++ views/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var bel = require('bel')
+
+module.exports = function index (state) {
+  return bel`<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>${state.title} - ${state.project}</title>
+    <link rel="shortcut icon"type="image/x-icon" href="data:image/x-icon;,">
+  </head>
+  <body>
+    <h1>${state.title}</h1>
+    <p>${state.description}</p>
+    <ul>${state.menu.map((item) => {
+      return bel`<li><a href="${item.link}">${item.text}</a></li>`
+    })}
+    </ul>
+    <hr>
+  </body>
+</html>`
+}
